0186885afad54ffcd5fcadafbc7c265aaa7d9a4c
ozcanovunc <ozcan_ovunc@hotmail.com>
2016-01-17 00:08:51 +0200

    Updated pedestrian detection

--- pedestrian-detection/Source.cpp
+++ pedestrian-detection/Source.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
-#define INPUT_VIDEO_FILE "video.avi"
+#define INPUT_VIDEO_FILE "pedestrian.mp4"
 #define WINDOW_NAME "WINDOW"
 
 // Reference: https://github.com/Itseez/opencv/blob/master/samples/cpp/train_HOG.cpp
@@ -37,13 +34,12 @@ int main(int argc, char** argv)
 	while (true) {
 
 		cap >> mSrc;
-		//hog.detectMultiScale(mSrc, vDetected, 0, Size(8, 8), Size(32, 32), 1.05, 2);
-		hog.detectMultiScale(mSrc, vDetected);
+		hog.detectMultiScale(mSrc, vDetected, 0, Size(8, 8), Size(32, 32), 1.05, 2);
 		draw_locations(mSrc, vDetected, Scalar(0, 255, 0));
 
 		imshow(WINDOW_NAME, mSrc);
 		waitKey(10);
 	}
-	
+
 	return 0;
 }
